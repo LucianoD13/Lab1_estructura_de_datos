cee56626a45084bdcc014eaafae2926669f5f372
Your Name <you@example.com>
2026-03-20 18:24:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -53,10 +53,10 @@ retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
    int suma = 0;
-   int elemento = front(L);
+   int elemento = Front(L);
    while(elemento != NULL){
-      suma += int(elemento);
-      elemento = next(L);
+      suma += elemento;
+      elemento = Next(L);
    }
    return suma;
 }
